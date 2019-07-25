282266570b95f70dd40b08cc595fb1727cf575af
tfgui <tfgui@ocube.co.kr>
2019-07-25 10:18:56 +0900

    open action

--- TestWidgetApp/testmainwindow.cpp
+++ TestWidgetApp/testmainwindow.cpp
@@ -1,5 +1,8 @@
 #include "testmainwindow.h"
 #include "ui_testmainwindow.h"
+#include <QFileDialog>
+#include <QFile>
+#include <QDebug>
 
 TestMainWindow::TestMainWindow(QWidget *parent) :
     QMainWindow(parent),
@@ -11,4 +14,26 @@ TestMainWindow::TestMainWindow(QWidget *parent) :
 TestMainWindow::~TestMainWindow()
 {
     delete ui;
+}
+
+void TestMainWindow::on_actionOpen_triggered()
+{
+    QString filename = QFileDialog::getOpenFileName( this, tr("Open file"), ".", tr("Text files (*.txt)"));
+    QFile file(filename);
+    if (!file.open((QFile::ReadOnly | QFile::Text))) {
+        qDebug() << "fail : open file " << filename;
+        return;
+    }
+
+    QTextStream in(&file);
+    QString data = in.readAll();
+    ui->textEdit->setText(data);
+
+    this->setFileName(filename);
+}
+
+void TestMainWindow::setFileName(QString filename)
+{
+    m_filename = filename;
+    this->setWindowTitle("TestMainWindow " + m_filename );
 }
--- TestWidgetApp/testmainwindow.h
+++ TestWidgetApp/testmainwindow.h
@@ -15,8 +15,15 @@ public:
     explicit TestMainWindow(QWidget *parent = 0);
     ~TestMainWindow();
 
+private slots:
+    void on_actionOpen_triggered();
+
 private:
     Ui::TestMainWindow *ui;
+
+public:
+    void setFileName(QString filename);
+    QString m_filename;
 };
 
 #endif // TESTMAINWINDOW_H
